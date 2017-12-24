eb9f3f35ee3de21137a7196230f284615e4d3b3b
jaj2610 <jaj2610@gmail.com>
2017-12-24 01:36:15 -0500

    Display and store search results data
     - Format pictures and basic info similar to vehicle search site
    
     - store pageData in state (total count, page count, total count
     formatted)
    
     - link to details page with vehicle id

--- src/components/searchpage.js
+++ src/components/searchpage.js
@@ -6,7 +6,7 @@ class Searchpage extends Component {
     return (
       <div>
         <h2>Search Page</h2>
-        <SearchResults />
+          <SearchResults />
       </div>
     );
   }
--- src/components/searchresults.js
+++ src/components/searchresults.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import './searchresults.css';
 
 class SearchResults extends Component {
   constructor() {
     super();
     this.state = {
-      pageNumber: 1,
-      displayedVehicles: []
+      displayedVehicles: [],
+      pageData: {}
     }
   }
 
@@ -14,24 +15,64 @@ class SearchResults extends Component {
     .then(results => {
       return results.json();
     }).then(data => {
-      let vehicles = data.records.map((vehicle) => {
-        return(
-          <div key={vehicle.id}>
-            <img src={vehicle.primary_photo_url} />
+      let vehicleData = {
+        count: data.total_count,
+        count_formatted: data.total_count_formatted,
+        page_count: data.page_count
+      };
 
+      let vehicles = data.records.map((vehicle) => {
+        let backgroundStyle = {
+          backgroundImage: 'url(' + vehicle.primary_photo_url + ')'
+        };
+        let vehicleLink = "/detailspage#id=" + vehicle.id;
+        return(
+          <div key={vehicle.id} className="vehicle-item-view">
+          <a href={vehicleLink} className="AL-vehicle-item -small-md -stacked-sm -stacked-xs -popout-action ">
+            <div className="photo">
+              <div className="AL-vehicle-photo">
+                <div className="viewed-overlay hidden"></div>
+                <div className="overlay-menu" data-menu="">
+                  <div className="item -favorite -results-gtsm " data-action="favorite"></div>
+                </div>
+                <div className="photo" style={backgroundStyle}></div>
+              </div>
+            </div>
+
+            <div className="details">
+              <h3 className="AL-vehicle-primary-details -small-sm -small-xs">
+                <div className="description">
+                  <div className="headline">{vehicle.year} {vehicle.make} {vehicle.model}</div>
+                  <div className="subtext">
+                      {vehicle.mileage_humanized}
+                  </div>
+                </div>
+                <div className="pricing">
+                  <div className="headline">{vehicle.price_mobile}</div>
+                </div>
+              </h3>
+
+              <div className="AL-vehicle-secondary-details -small-sm -small-xs" data-menu="">
+                <div className="location">Location: ({vehicle.lat},{vehicle.lon})</div>
+              </div>
+            </div>
+          </a>
           </div>
         )
       })
-      this.setState({pageNumber: 1, displayedVehicles: vehicles})
+
+      this.setState({displayedVehicles: vehicles, pageData: vehicleData})
       console.log("vehicles:", this.state.displayedVehicles);
+      console.log("vehicleData:", this.state.pageData);
     })
   }
 
   render() {
     return (
-      <div className="container 2">
-        <div className="container 1">
-          {this.state.displayedVehicles}
+      <div className="search-column">
+        <div className="search-header">Results: {this.state.pageData.count_formatted}</div>
+        <div id="vehicle-list" className="vehicle-list" style={{display: "block"}}>
+            {this.state.displayedVehicles}
         </div>
       </div>
     );
