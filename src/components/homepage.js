92fcb2f832929654f33bdbeb49df39a355c558bb
jaj2610 <jaj2610@gmail.com>
2017-12-24 03:39:07 -0500

    Add simple form in homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,13 +1,52 @@
 import React, { Component } from "react";
 
 class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      minPrice: '',
+      maxPrice: '',
+      page: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+
+    this.setState({
+      [name]: target.value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log('A search was submitted: ' + JSON.stringify(this.state));
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
         <h2>Home</h2>
-        <p>Home page</p>
 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Min Price:
+            <input type="number" name="minPrice" value={this.state.minPrice} onChange={this.handleChange} />
+          </label><br/>
+          <label>
+            Max Price:
+            <input type="number" name="maxPrice" value={this.state.maxPrice} onChange={this.handleChange} />
+          </label><br/>
+          <label>
+            Page Number:
+            <input type="number" name="page" value={this.state.page} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Search" />
+        </form>
       </div>
     );
   }
