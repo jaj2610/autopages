e126a66e8947b7df148ccd718b598918c02e9372
jaj2610 <jaj2610@gmail.com>
2017-12-28 20:12:16 -0500

    Add basic info + styling to details page

--- src/components/detailspage.js
+++ src/components/detailspage.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 
+import './searchresults.css';
+
 class Detailspage extends Component {
   constructor(props) {
     super(props);
@@ -31,9 +33,47 @@ class Detailspage extends Component {
   }
 
   render() {
+    let backgroundStyle = {
+      backgroundImage: 'url(' + this.state.vehicle.primary_photo_url + '), url(/not_found.jpg)'
+    };
 
     return (<div>
       <h2>Details Page</h2>
+
+      <div key={this.state.vehicle.id} className="vehicle-item-detail">
+        <div className="AL-vehicle-item -small-md -stacked-sm -stacked-xs -popout-action ">
+          <div className="photo">
+            <div className="AL-vehicle-photo">
+              <div className="viewed-overlay hidden"></div>
+              <div className="overlay-menu" data-menu="">
+                <div className="item -favorite -results-gtsm " data-action="favorite"></div>
+              </div>
+              <div className="photo" style={backgroundStyle}></div>
+            </div>
+          </div>
+
+          <div className="details">
+            <h3 className="AL-vehicle-primary-details -small-sm -small-xs">
+              <div className="description">
+                <div className="headline">{this.state.vehicle.year}&nbsp;
+                  {this.state.vehicle.make}&nbsp;
+                  {this.state.vehicle.model}</div>
+                <div className="subtext">
+                  {this.state.vehicle.mileage_humanized}
+                </div>
+              </div>
+              <div className="pricing">
+                <div className="headline">{this.state.vehicle.price_mobile}</div>
+              </div>
+            </h3>
+
+            <div className="AL-vehicle-secondary-details -small-sm -small-xs" data-menu="">
+              <div className="location">Location: ({this.state.vehicle.lat},{this.state.vehicle.lon})</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {JSON.stringify(this.state.vehicle)}
     </div>);
   }
